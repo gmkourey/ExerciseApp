c1509af485b6534ff16fd105ba71ce1c8df5aec9
Grace Andrizzi <graceandrizzi@Graces-MacBook-Pro.local>
2018-08-16 11:13:59 -0700

    calculations

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ var weightKg = weightPounds * .454;
 var ree = 0;
 var sex = $("#sex").val();
 var age = $("#age").val();
-var activityLevel = $("#activityLevel").val();
+var activityLevel = $("#activityLevel").val(); 
+var protein = 0;
+var fat = 0;
+var carbs = 0;
+// varbiables for activity levels: couchPotatoe, moderatelyActive, highlyActive, triathlonRunner
 
 
 //calculate variables based on inputs
@@ -35,3 +39,23 @@ if (sex === "male") {
     ree = (weightKg * 10) + (6.25 * heightInCm) - (age * 5) - 161;
 }
 
+switch(activityLevel) {
+    case "couchPotatoe": 
+    ree *= 1.2;
+    break;
+    case "moderatelyActive":
+    ree *= 1.375;
+    break;
+    case "highlyActive":
+    ree *= 1.55;
+    break;
+    case "triathlonRunner":
+    ree *= 1.725;
+    break;
+};
+
+ree = Math.round(ree);
+
+protein = Math.round(ree * .35);
+fat = Math.round(ree * .2);
+carbs = Math.round(ree * .45);
