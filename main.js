aa5ea799156c0d5810d2cb919dc55c072018baa2
Grace Andrizzi <graceandrizzi@Graces-MacBook-Pro.local>
2018-08-16 10:48:57 -0700

    initial js

--- main.js
+++ main.js
@@ -6,8 +6,14 @@
 var heightFeetInput = $("#heightFeetInput").val();
 var heightInchesInput = $("#heightInchesInput").val();
 var heightInInches = (parse(heightFeetInput) * 12) + parse(heightInInches);
-var weight = $("#weightInput").val();
-var gender = [];
+var heightInCm = heightInInches * 2.54;
+var weightPounds = $("#weightInput").val();
+var weightKg = weightPounds * .454;
+var ree = 0;
+var sex = $("#sex").val();
+var age = $("#age").val();
+var activityLevel = $("#activityLevel").val();
+
 
 //calculate variables based on inputs
 var BMI = ((weight * 705)/heightInInches)/heightInInches;
@@ -21,4 +27,11 @@ if (BMI >= 18.5 && BMI <= 25) ... = "Normal";
 if (BMI >= 25 && BMI <= 30) ... = "Obese";
 if (BMI > 30) ... = "Overweight";           
 
+
+// calories 
+if (sex === "male") {
+    ree = (weightKg * 10) + (6.25 * heightInCm) - (age * 5) + 5;
+} else {
+    ree = (weightKg * 10) + (6.25 * heightInCm) - (age * 5) - 161;
+}
 
