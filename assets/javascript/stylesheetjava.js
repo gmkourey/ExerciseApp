9a1b784e778fad7acddced3b5cadcc06da9949bd
Ad_Victoriam <keeper24@gmail.com>
2018-08-16 21:18:06 -0700

    added a working js sheet and a loading icon. also added some functionality.

--- assets/javascript/stylesheetjava.js
+++ assets/javascript/stylesheetjava.js
@@ -1,1 +1,19 @@
 // style sheet working JS
+// sudo code, user selects what they want to fill out
+// 
+$(document).ready(function(){
+    $("#form1").hide();
+    $(".fa").hide();
+    $(".btn-info").on('click', function(){
+        $('#form1').toggle();
+        $(".btn-info").text("Fill out the Form Below")
+    });
+    $("#submitInfo").on('click',function(event){
+        event.preventDefault();
+        $(".btn-info").hide();
+        $("#form1").toggle();
+        // this should load till the back end data form is ready, or after a set amount of time. 
+        $(".fa").toggle();
+
+    });
+});
