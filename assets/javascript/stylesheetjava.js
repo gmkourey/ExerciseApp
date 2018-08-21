f061a17b7f4684d2e30b2badaf7127f7ca54f2b4
ad-victoriam-101 <keeper24@gmail.com>
2018-08-21 11:19:43 -0700

    added footer code with popover links

--- assets/javascript/stylesheetjava.js
+++ assets/javascript/stylesheetjava.js
@@ -1,18 +1,8 @@
 // style sheet working JS
 // sudo code, user selects what they want to fill out
 // 
-$(document).ready(function(){
-    // $("#form1").hide();
-    $(".fa-spin").hide();
-    $(".chicken").on('click', function(){
-        $('#form1').show().children().show();
-        $(".btn-info").text("Pick Your Goal.")
-    });
-    $("#submitInfo").on('click',function(event){
-        event.preventDefault();
-        $(".btn-info").hide();
-        $("#form1").hide();
-        // this should load till the back end data form is ready, or after a set amount of time. 
-        $(".fa-spin").show();
-    });
+// popovers Initialization
+$(function () {
+    $('[data-toggle="popover"]').popover({html:true})
 });
+
